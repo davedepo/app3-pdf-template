e14cb1720aea66b175921ba384f53d1d5c45cea5
Pratik <pratik.dave@msn.com>
2024-05-27 17:44:08 +0530

    D24: Exercise: Add lines

---
+++ main_lines.py
@@ -0,0 +1,42 @@
+from fpdf import FPDF
+import pandas as pd
+
+# to create a pdf instance/object from PDF class
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
+
+# read .csv file as dataframe object
+df = pd.read_csv("topics.csv")
+
+# create for-loop for each instance of row object
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L",
+             ln=1, border=0)
+    for i in range(20, 288, 10):
+        pdf.line(10, i, 200, i)
+
+    # to set the footer - pgs with topic title
+    pdf.ln(268)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=0, txt=row["Topic"], align="R",
+             ln=1, border=0)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
+        for y in range(20, 288, 10):
+            pdf.line(10, y, 200, y)
+
+        # to set the footer - pgs added after title
+        pdf.ln(280)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=0, txt=row["Topic"], align="R",
+                 ln=1, border=0)
+
+pdf.output("output_lines.pdf")
